3ffc105a03cf41f05ad92159c519a5ef30ac4936
Thijs <thijsvanmarle@gmail.com>
2023-02-07 12:44:54 +0100

    Router Fixed

--- VBR-App/src/app/app.module.ts
+++ VBR-App/src/app/app.module.ts
@@ -1,21 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyContainerComponent } from './body-container/body-container.component';
+import { ContactComponent } from './body-container/contact/contact.component';
+import { AgendaComponent } from './body-container/agenda/agenda.component';
+import { ArtistsComponent } from './body-container/artists/artists.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'agenda', component: AgendaComponent},
+  {path: 'artists', component: ArtistsComponent},
+  {path: 'contact', component: ContactComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyContainerComponent
+    BodyContainerComponent,
+    ContactComponent,
+    AgendaComponent,
+    ArtistsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
